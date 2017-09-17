e5ef3a4f1085752ff82addc1164e53acfa440f73
Jschles1 <jschles1@ramapo.edu>
2017-09-17 17:59:04 -0400

    Fixed syntax errors in valid_attributes, added tests for creating a new exercise resource upon a valid request to the API

--- spec/requests/exercises_spec.rb
+++ spec/requests/exercises_spec.rb
@@ -25,13 +25,28 @@ RSpec.describe 'Exercises API', type: :request do
     let(:valid_attributes) {
       {
         exercise: {
-          name: Faker::Lorem.word
-          reps: Faker::Number.between(1, 30)
-          sets: Faker::Number.between(1, 10)
-          rest_period: { Faker::Number.between(10, 1000)
+          name: Faker::Lorem.word,
+          reps: Faker::Number.between(1, 30),
+          sets: Faker::Number.between(1, 10),
+          rest_period: Faker::Number.between(10, 1000)
         }
       }
     }
+
+    context 'upon a valid request' do
+      before { post "/api/workouts/#{workout_id}/exercises" }
+
+      it 'returns a status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'creates a new exercise and persists it to the database' do
+        expect(json[:name]).to eq(valid_attributes[:exercise][:name])
+        expect(json[:reps]).to eq(valid_attributes[:exercise][:reps])
+        expect(json[:sets]).to eq(valid_attributes[:exercise][:sets])
+        expect(json[:rest_period]).to eq(valid_attributes[:exercise][:rest_period])
+      end
+    end
   end
 
 end
