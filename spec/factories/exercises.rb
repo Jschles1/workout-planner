1c7d47643b1bd05ac1e2067e5de3e1000dfda470
Jschles1 <jschles1@ramapo.edu>
2017-09-16 22:13:50 -0400

    Created Exercise factory

---
+++ spec/factories/exercises.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :exercises do
+    name { Faker::Lorem.word }
+    workout_id nil
+    reps { Faker::Number.between(1, 30) }
+    sets { Faker::Number.between(1, 10) }
+    rest_period { Faker::Number.between(10, 1000) }
+  end
+end
