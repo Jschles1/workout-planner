22f15aff841e580d2d06e2da10f76bd7277de440
Jschles1 <jschles1@ramapo.edu>
2017-09-17 22:11:29 -0400

    Imported dependencies into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.css'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
