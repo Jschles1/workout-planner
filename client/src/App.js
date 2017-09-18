5f806aae4375e74701582a7ec77e4606486caa57
Jschles1 <jschles1@ramapo.edu>
2017-09-18 18:46:58 -0400

    Imported Navbar element & added it to App render(). Enclosed Navbar & route into single child element

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Route exact path="/" component={Welcome} />
+          <div>
+            <Navbar/>
+            <Route exact path="/" component={Welcome} />
+          </div>
         </Router>
       </div>
     );
