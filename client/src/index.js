ff8ac7c21c4e29720aea194bce31567bcaddf4f8
Jschles1 <jschles1@ramapo.edu>
2017-09-17 22:38:23 -0400

    Defined store, implemented Provider component

--- client/src/index.js
+++ client/src/index.js
@@ -9,5 +9,17 @@ import rootReducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)  
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App /> 
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
