8d598bd6c53010c45c74921923ea15740a764cf2
Jschles1 <jschles1@ramapo.edu>
2017-09-18 18:17:36 -0400

    Temporarily commented out store & reducers in index.js, created & exported Welcome component, imported Welcome component & changed Switch to Router component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
+        <Router>
           <Route exact path="/" component={Welcome} />
-        </Switch>
+        </Router>
       </div>
     );
   }
