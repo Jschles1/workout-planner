30fd75c3c12611026127b93703e51302d646cb10
Jschles1 <jschles1@ramapo.edu>
2017-09-19 18:35:02 -0400

    Changed workoutType to workout_type, imported actions from workoutActions & bindActionCreators from redux

--- client/src/components/WorkoutsForm.js
+++ client/src/components/WorkoutsForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as actions from '../actions/workoutActions';
+import { bindActionCreators } from 'redux';
 import { Form, Button } from 'semantic-ui-react';
 
 export default class WorkoutsForm extends React.Component {
@@ -7,7 +9,7 @@ export default class WorkoutsForm extends React.Component {
 
     this.state = {
       title: '',
-      workoutType: ''
+      workout_type: ''
     }
   }
 
@@ -29,7 +31,7 @@ export default class WorkoutsForm extends React.Component {
           </Form.Field>
           <Form.Field>
             <label>Workout Type:</label>
-            <input name="workoutType" onChange={(event) => this.handleOnChange(event)} placeholder="Workout Type"/>
+            <input name="workout_type" onChange={(event) => this.handleOnChange(event)} placeholder="Workout Type"/>
           </Form.Field>
           <Button type="submit">Submit</Button>
         </Form>
