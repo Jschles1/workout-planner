045777dc8c5023f2dfc2c4253c0f8a74c3e8b8ae
Jschles1 <jschles1@ramapo.edu>
2017-09-18 22:28:49 -0400

    Imported semantic Form & button elements, created constructor function & set initial state

--- client/src/components/WorkoutsForm.js
+++ client/src/components/WorkoutsForm.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
 
 export default class WorkoutsForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      workoutType: ''
+    }
+  }
   render() {
     return(
       <div></div>
