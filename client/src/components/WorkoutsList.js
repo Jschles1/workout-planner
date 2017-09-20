9c4f621b2115fabe48a25d5573047a27e8b542cf
Jschles1 <jschles1@ramapo.edu>
2017-09-19 21:22:45 -0400

    Added componentDidMount to WorkoutsList that fetches all workouts from API. Built out basic functionality for WorkoutsList that currently lists titles of all workouts in list format

--- client/src/components/WorkoutsList.js
+++ client/src/components/WorkoutsList.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { List, Button } from 'semantic-ui-react';
 
 export default class WorkoutsList extends React.Component {
   render() {
+    const renderWorkouts = this.props.workouts.map(workout => {
+      return(
+        <List.Item key={workout.id}>
+          {workout.title}
+        </List.Item>
+      );
+    })
+
     return(
-      <div></div>
+      <div>
+        <h1>Workouts:</h1>
+        <List size="massive">
+          {(this.props.workouts) ? renderWorkouts : null}
+        </List>
+      </div>
     );
   }
 }
--- client/src/components/WorkoutsPage.js
+++ client/src/components/WorkoutsPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions/workoutActions';
+import { bindActionCreators } from 'redux';
 import WorkoutsList from './WorkoutsList'
 // Responsible for connecting to our store and loading our list of workouts
 class WorkoutsPage extends React.Component {
+
+  componentDidMount() {
+    this.props.actions.fetchWorkouts();
+  }
+  
   render() {
     return(
       <div>
@@ -16,4 +23,8 @@ const mapStateToProps = (state) => {
   return { workouts: state.workouts };
 }
 
-export default connect(mapStateToProps)(WorkoutsPage);
+const mapDispactchToProps = (dispatch) => {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispactchToProps)(WorkoutsPage);
