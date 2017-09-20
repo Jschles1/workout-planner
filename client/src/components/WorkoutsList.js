6c1230ed147e580ea67d8f0ecba916a4ee4fedc9
Jschles1 <jschles1@ramapo.edu>
2017-09-19 21:44:20 -0400

    Changed WorkoutsList from List to Table format

--- client/src/components/WorkoutsList.js
+++ client/src/components/WorkoutsList.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { List, Button } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 
 export default class WorkoutsList extends React.Component {
   render() {
     const renderWorkouts = this.props.workouts.map(workout => {
       return(
-        <List.Item key={workout.id}>
-          {workout.title}
-        </List.Item>
+        <Table.Row key={workout.id}>
+          <Table.Cell>{workout.title}</Table.Cell>
+          <Table.Cell>{workout.workout_type}</Table.Cell>
+        </Table.Row>
       );
     })
 
     return(
       <div>
         <h1>Workouts:</h1>
-        <List size="massive">
-          {(this.props.workouts) ? renderWorkouts : null}
-        </List>
+        <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell></Table.HeaderCell>
+                <Table.HeaderCell></Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {(this.props.workouts) ? renderWorkouts : null}
+            </Table.Body>
+        </Table>
       </div>
     );
   }
