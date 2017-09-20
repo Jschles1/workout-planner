c27f54b6a2b842f8f0c0facaa41f02d7a6f2f471
Jschles1 <jschles1@ramapo.edu>
2017-09-19 22:55:32 -0400

    Reformatted Table, added View Workout & Delete Workout buttons

--- client/src/components/WorkoutsList.js
+++ client/src/components/WorkoutsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Table, Button } from 'semantic-ui-react';
+import { Table, Button, Icon } from 'semantic-ui-react';
 
 export default class WorkoutsList extends React.Component {
   render() {
@@ -9,6 +9,12 @@ export default class WorkoutsList extends React.Component {
         <Table.Row key={workout.id}>
           <Table.Cell>{workout.title}</Table.Cell>
           <Table.Cell>{workout.workout_type}</Table.Cell>
+          <Table.Cell>
+            <Button content="View Workout" color="green" icon="right arrow" labelPosition="right"/>
+          </Table.Cell>
+          <Table.Cell>
+            <Button content="Delete Workout" color="red" icon="remove" labelPosition="right"/>
+          </Table.Cell>
         </Table.Row>
       );
     })
@@ -16,13 +22,12 @@ export default class WorkoutsList extends React.Component {
     return(
       <div>
         <h1>Workouts:</h1>
-        <Table>
+        <Table celled>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>Title</Table.HeaderCell>
-                <Table.HeaderCell>Type</Table.HeaderCell>
-                <Table.HeaderCell></Table.HeaderCell>
-                <Table.HeaderCell></Table.HeaderCell>
+                <Table.HeaderCell>Title:</Table.HeaderCell>
+                <Table.HeaderCell>Type:</Table.HeaderCell>
+                <Table.HeaderCell colSpan="2">Options:</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
