6b520fd389ef251589d027630a920178f971a057
Jschles1 <jschles1@ramapo.edu>
2017-09-20 22:11:41 -0400

    Connect WorkoutShow to store via connect() & mapStateToProps()

--- client/src/components/WorkoutShow.js
+++ client/src/components/WorkoutShow.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export default class WorkoutShow extends React.Component {
+class WorkoutShow extends React.Component {
   render() {
     return(
-      <div></div>
+      <div>
+        <h1>{this.props.workout.title}</h1>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const workout = state.workouts.find(workout => workout.id === parseInt(ownProps.match.params.workoutId), 10);
+  
+  if (workout) {
+    return { workout: workout }
+  } else {
+    return { workout: {} }
+  }
+}
+
+export default connect(mapStateToProps, {})(WorkoutShow);
