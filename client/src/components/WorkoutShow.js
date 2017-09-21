0319533edeed5a211e0e9eb0c1f99f4036386eee
Jschles1 <jschles1@ramapo.edu>
2017-09-21 17:37:36 -0400

    Imported & implemented ExerciseList, added exercises prop to mapStateToProps()

--- client/src/components/WorkoutShow.js
+++ client/src/components/WorkoutShow.js
@@ -3,12 +3,14 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/exerciseActions';
 import { bindActionCreators } from 'redux';
 import ExerciseForm from './ExerciseForm';
+import ExerciseList from './ExerciseList';
 
 class WorkoutShow extends React.Component {
   render() {
     return(
       <div>
         <h1>{this.props.workout.title}</h1>
+        <ExerciseList exercises={this.props.exercises}/>
         <ExerciseForm workoutId={this.props.workout.id}/>
       </div>
     );
@@ -19,7 +21,7 @@ const mapStateToProps = (state, ownProps) => {
   const workout = state.workouts.find(workout => workout.id === parseInt(ownProps.match.params.workoutId), 10);
   
   if (workout) {
-    return { workout: workout }
+    return { workout: workout, exercises: state.exercises }
   } else {
     return { workout: {} }
   }
