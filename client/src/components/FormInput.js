90c65e39cc5ac25b97292220a7c6efa30edb3c1a
Jschles1 <jschles1@ramapo.edu>
2017-09-21 19:24:54 -0400

    Created & exported FormInput Component

---
+++ client/src/components/FormInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Form, Label, Input } from 'semantic-ui-react';
+
+export default class FormInput extends React.PureComponent {
+  render() {
+    const { labelName, inputName, inputType, inputValue, inputSize, inputPlaceholder, updateField } = this.props;
+    return(
+      <Form.Field>
+        <Label color="black" pointing="below">{labelName}</Label>
+        <Input 
+          value={inputValue} 
+          size={inputSize} 
+          name={inputName} 
+          onChange={updateField} 
+          placeholder={inputPlaceholder}
+        />
+      </Form.Field>
+    );
+  }
+}
