781ffbce9712b003d7d2ffa6626900b2ba443ae4
Jschles1 <jschles1@ramapo.edu>
2017-09-20 22:21:38 -0400

    Created ExerciseForm

---
+++ client/src/components/ExerciseForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/workoutActions';
+import { bindActionCreators } from 'redux';
+import { Form, Button, Label, Icon } from 'semantic-ui-react';
+
+class ExerciseForm extends React.Component {
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
