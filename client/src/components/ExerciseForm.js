940bc7d3d4bbe173731fb59ef3a8053b90a58312
Jschles1 <jschles1@ramapo.edu>
2017-09-20 22:37:01 -0400

    Added inputs to ExerciseForm

--- client/src/components/ExerciseForm.js
+++ client/src/components/ExerciseForm.js
@@ -15,10 +15,31 @@ class ExerciseForm extends React.Component {
       rest_period: ''
     }
   }
-  
+
   render() {
     return(
-      <div></div>
+      <div>
+        <h3>Add an Exercise to this Workout:</h3>
+        <Form>
+          <Form.Field>
+            <Label color="blue" pointing="below">Name:</Label>
+            <input name="name" onChange={(event) => this.handleOnChange(event)} placeholder="Name"/>
+          </Form.Field>
+          <Form.Field>
+            <Label color="blue" pointing="below">Repetitions:</Label>
+            <input name="reps" onChange={(event) => this.handleOnChange(event)} placeholder="Repetitions"/>
+          </Form.Field>
+          <Form.Field>
+            <Label color="blue" pointing="below">Number Of Sets:</Label>
+            <input name="sets" onChange={(event) => this.handleOnChange(event)} placeholder="Number Of Sets"/>
+          </Form.Field>
+          <Form.Field>
+            <Label color="blue" pointing="below">Rest Period Between Sets:</Label>
+            <input name="rest_period" onChange={(event) => this.handleOnChange(event)} placeholder="Rest Period Between Sets"/>
+          </Form.Field>
+          <Button color="blue" content="Submit" type="submit" icon="right arrow" labelPosition="right"/>
+        </Form>
+      </div>
     );
   }
 }
