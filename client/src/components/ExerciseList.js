b81042fa5e49cb494f7db929bbc5bef34469e447
Jschles1 <jschles1@ramapo.edu>
2017-09-21 17:31:12 -0400

    Created & exported ExerciseList

---
+++ client/src/components/ExerciseList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Button, Icon } from 'semantic-ui-react';
+
+export default class ExerciseList extends React.Component {
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
