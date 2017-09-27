c11938956aa374ef42bd4ec2cbb64a768ee12749
Jschles1 <jschles1@ramapo.edu>
2017-09-27 17:23:35 -0400

    Added description for Help button in Welcome. Added info_url & Add To Workouts List button in renderSuggestedWorkouts

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,6 +7,7 @@ const Welcome = () => {
       <h1>Welcome to my Workout Planner App!</h1>
       <h3>This app can be used to create custom workouts, where you can add and remove exercises seamlessly.</h3>
       <h3>To get started, click the "Create New Workout" button above.</h3>
+      <h3>If you're new to fitness and don't know what to do, click "Help" to get a list of premade suggested workouts for beginners.</h3>
     </div>
   );
 }
