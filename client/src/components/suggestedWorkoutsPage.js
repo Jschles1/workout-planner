168372efec91d3a5a5f2e135264d8aed6c13d721
Jschles1 <jschles1@ramapo.edu>
2017-09-26 22:15:36 -0400

    Created suggestedWorkoutsPage component & changed numericality validation in workout model to allow rest_periods of 0

---
+++ client/src/components/suggestedWorkoutsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/workoutActions';
+import { bindActionCreators } from 'redux';
+
+class suggestedWorkoutsPage extends React.Component {
+  return() {
+    <div></div>
+  }
+}
