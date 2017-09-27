2e078351bb07b18bc8846555fe4329454badc546
Jschles1 <jschles1@ramapo.edu>
2017-09-27 02:34:24 -0400

    Added table header format to suggestedWorkoutList

--- client/src/components/suggestedWorkoutsList.js
+++ client/src/components/suggestedWorkoutsList.js
@@ -3,5 +3,25 @@ import { Link } from 'react-router-dom';
 import { Table, Button, Icon } from 'semantic-ui-react';
 
 const suggestedWorkoutsList = (props) => {
-  
+  const { addWorkout, workouts } = props;
+
+
+
+  return(
+    <div>
+      <h1>Workouts:</h1>
+      <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Title:</Table.HeaderCell>
+              <Table.HeaderCell>Type:</Table.HeaderCell>
+              <Table.HeaderCell>Options:</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {(workouts) ? renderWorkouts : null}
+          </Table.Body>
+      </Table>
+    </div>
+  );
 }
