72924592981304138b8a7f85047d05eb13da76b2
Jschles1 <jschles1@ramapo.edu>
2017-09-26 21:41:53 -0400

    Added seed data for first 4 suggested workouts (large commit)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Workout.create(id: 1, 
+              title: "Starting Strength Workout A", 
+              workout_type: "Weightlifting", 
+              suggested: true,
+              info_url: "https://www.bodybuilding.com/fun/novice-lifters-start-here.html")
+  Exercise.create(workout_id: 1, name: "Barbell Squat", reps: 5, sets: 3, rest_period: 4)
+  Exercise.create(workout_id: 1, name: "Bench Press", reps: 5, sets: 3, rest_period: 4)
+  Exercise.create(workout_id: 1, name: "Deadlift", reps: 5, sets: 1, rest_period: 0)
+
+Workout.create(id: 2, 
+              title: "Starting Strength Workout B", 
+              workout_type: "Weightlifting", 
+              suggested: true,
+              info_url: "https://www.bodybuilding.com/fun/novice-lifters-start-here.html")
+  Exercise.create(workout_id: 2, name: "Barbell Squat", reps: 5, sets: 3, rest_period: 4)
+  Exercise.create(workout_id: 2, name: "Overhead Press", reps: 5, sets: 3, rest_period: 4)
+  Exercise.create(workout_id: 2, name: "Deadlift", reps: 5, sets: 1, rest_period: 0)
+
+Workout.create(id: 3, 
+              title: "Jason Blaha's 5x5 Novice Program Workout A",
+              workout_type: "Full Body",
+              suggested: true,
+              info_url: "https://www.muscleandstrength.com/workouts/jason-blaha-ice-cream-fitness-5x5-novice-workout")
+  Exercise.create(workout_id: 3, name: "Squats", reps: 5, sets: 5, rest_period: 5)
+  Exercise.create(workout_id: 3, name: "Bench Press", reps: 5, sets: 5, rest_period: 5)
+  Exercise.create(workout_id: 3, name: "Bent Over Row", reps: 5, sets: 5, rest_period: 5)
+  Exercise.create(workout_id: 3, name: "Barbell Shrugs", reps: 8, sets: 3, rest_period: 2)
+  Exercise.create(workout_id: 3, name: "Tricep Extensions", reps: 8, sets: 3, rest_period: 2)
+  Exercise.create(workout_id: 3, name: "Straight Bar or Inline Curls", reps: 8, sets: 3, rest_period: 2)
+  Exercise.create(workout_id: 3, name: "Hyperextensions with Plate", reps: 10, sets: 2, rest_period: 2)
+  Exercise.create(workout_id: 3, name: "Cable Crunches", reps: 10, sets: 3, rest_period: 2)
+
+Workout.create(id: 4, 
+              title: "Jason Blaha's 5x5 Novice Program Workout B",
+              workout_type: "Full Body",
+              suggested: true,
+              info_url: "https://www.muscleandstrength.com/workouts/jason-blaha-ice-cream-fitness-5x5-novice-workout")
+  Exercise.create(workout_id: 4, name: "Squats", reps: 5, sets: 5, rest_period: 5)
+  Exercise.create(workout_id: 4, name: "Deadlift", reps: 5, sets: 1, rest_period: 0)
+  Exercise.create(workout_id: 4, name: "Standing Press", reps: 5, sets: 5, rest_period: 5)  
+  Exercise.create(workout_id: 4, name: "Bent Over Row (10% lighter than workout A)", reps: 5, sets: 5, rest_period: 5)
+  Exercise.create(workout_id: 4, name: "Close Grip Bench Press", reps: 8, sets: 3, rest_period: 2)
+  Exercise.create(workout_id: 4, name: "Straight Bar or Inline Curls", reps: 8, sets: 3, rest_period: 2)
+  Exercise.create(workout_id: 4, name: "Cable Crunches", reps: 10, sets: 3, rest_period: 2)   
+
