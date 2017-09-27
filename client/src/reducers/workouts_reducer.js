bbed359f9809c7be9e66bbafcadbf6871f32cbf9
Jschles1 <jschles1@ramapo.edu>
2017-09-27 00:36:56 -0400

    Added FETCH_SUGGESTED_WORKOUTS case, & modified FETCH_WORKOUTS case to only fetch workouts that have a suggested attribute of false

--- client/src/reducers/workouts_reducer.js
+++ client/src/reducers/workouts_reducer.js
@@ -1,7 +1,11 @@
 export default function workoutsReducer(state = [], action) {
   switch(action.type) {
     case 'FETCH_WORKOUTS':
-      return action.payload;
+      const workouts = action.payload.filter(workout => workout.suggested === false);
+      return workouts;
+    case 'FETCH_SUGGESTED_WORKOUTS':
+      const suggestedWorkouts = action.payload.filter(workout => workout.suggested === true);
+      return suggestedWorkouts;
     case 'ADD_WORKOUT':
       return state.concat(action.payload);
     case 'DELETE_WORKOUT':
@@ -10,3 +14,5 @@ export default function workoutsReducer(state = [], action) {
       return state;
   }
 }
+
+// On edit, 1st remove the old workout through filter by id, then add back the edited workout to state
