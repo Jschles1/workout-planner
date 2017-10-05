8ed9129dee046f3dea4dafe749ba8ad5ddb3441e
Jschles1 <jschles1@ramapo.edu>
2017-10-05 17:11:54 -0400

    Added relationships to serializers

--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,3 +1,4 @@
 class ExerciseSerializer < ActiveModel::Serializer
   attributes :id, :name, :reps, :sets, :rest_period, :workout_id
+  belongs_to :workout
 end
--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,3 +1,4 @@
 class WorkoutSerializer < ActiveModel::Serializer
   attributes :id, :title, :workout_type, :suggested, :info_url
+  has_many :exercises
 end
