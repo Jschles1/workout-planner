31831ef8a4249585c890720df728bef7e97fca20
Jschles1 <jschles1@ramapo.edu>
2017-10-05 17:29:09 -0400

    Added likes to workout_serializer & like validation for numericality in Workout model

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,4 +1,4 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :title, :workout_type, :suggested, :info_url
+  attributes :id, :title, :workout_type, :suggested, :info_url, :likes
   has_many :exercises
 end
