bb6aba42f6523d3cf3a1dc269a2f9cc71911785b
Jschles1 <jschles1@ramapo.edu>
2017-10-04 21:58:12 -0400

    Added cases for ADD_WORKOUT & ADD_EXERCISE in errorsReducer

--- client/src/reducers/errors_reducer.js
+++ client/src/reducers/errors_reducer.js
@@ -2,9 +2,13 @@ export default function errorsReducer(state = false, action) {
   switch (action.type) {
     case 'ERROR':
       return true;
-    case 'FETCH_EXERCISES':
+    case 'FETCH_WORKOUTS':
       return false;
     case 'FETCH_EXERCISES':
+      return false;
+    case 'ADD_WORKOUT':
+      return false;
+    case 'ADD_EXERCISE':
       return false;
     default:
       return state;
