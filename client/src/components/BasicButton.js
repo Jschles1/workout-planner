4e52abd07cb6206741bfb7bde850f75c45013b8d
Jschles1 <jschles1@ramapo.edu>
2017-12-07 23:23:02 -0500

    Completed BasicButton

--- client/src/components/BasicButton.js
+++ client/src/components/BasicButton.js
@@ -3,9 +3,38 @@ import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class BasicButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBeingHovered: false
+    }
+  }
+
+  invertColor = () => {
+    this.setState({
+      isBeingHovered: !this.state.isBeingHovered
+    })
+  }
+
   render() {
+    const { isFluid, color, click, textContent, link, url, type, icon, label, id } = this.props;
     return(
-      <Button />
+      <Button 
+        fluid={isFluid}
+        basic={!(this.state.isBeingHovered)}
+        color={color}
+        onClick={click}
+        onMouseOver={this.invertColor}
+        onMouseLeave={this.invertColor}
+        as={link ? link : null}
+        to={url ? url : null}
+        type={type ? type : null}
+        icon={icon ? icon : null}
+        labelPosition={label === "right" ? "right" : null}
+        id={id ? id : null}
+        content={textContent}
+      />
     );
   }
 }
