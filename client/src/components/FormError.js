64761c4596092755bb28b849c2b5681cc25b5c11
Jschles1 <jschles1@ramapo.edu>
2017-12-24 00:49:12 -0500

    Removed errorStyles variable, changed format to use implicit rendering

--- client/src/components/FormError.js
+++ client/src/components/FormError.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 // Stateless Component
-const FormError = () => {
-  const errorStyles = { color: 'red' }
-  return(
-    <p style={errorStyles}>Error: All inputs must be filled.</p>
-  );
-}
+const FormError = () => <p style={{color: 'red'}}>Error: All inputs must be filled.</p>
 
 export default FormError;
