bc36bbad4a8084980513455b1549217ecde312f8
Jschles1 <jschles1@ramapo.edu>
2017-12-24 00:46:45 -0500

    Changed format to use implicit rendering

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,24 +3,21 @@ import { Menu, Icon } from 'semantic-ui-react';
 import { NavLink, Link } from 'react-router-dom';
 
 // Stateless Component
-const Navbar = () => {
-  return (
-    <div>
-      <Menu inverted>
-        <Menu.Item header>Workout Planner</Menu.Item>
-        <Menu.Item as={Link} to='/'>
-          <Icon name="home" /> 
-        </Menu.Item>
-        <Menu.Item as={Link} to='/workouts' name="Workouts" />
-        <Menu.Item as={Link} to='/workouts/new' name="Create New Workout" />
-        <Menu.Item as={Link} to='/help' name="Help" />
-        <Menu.Item position="right">
-          &copy; John Schlesinger
-        </Menu.Item>
-      </Menu>
-      <br/>
-    </div>
-  );
-}
-
+const Navbar = () => 
+  <div>
+    <Menu inverted>
+      <Menu.Item header>Workout Planner</Menu.Item>
+      <Menu.Item as={Link} to='/'>
+        <Icon name="home" /> 
+      </Menu.Item>
+      <Menu.Item as={Link} to='/workouts' name="Workouts" />
+      <Menu.Item as={Link} to='/workouts/new' name="Create New Workout" />
+      <Menu.Item as={Link} to='/help' name="Help" />
+      <Menu.Item position="right">
+        &copy; John Schlesinger
+      </Menu.Item>
+    </Menu>
+    <br />
+  </div>
+  
 export default Navbar;
